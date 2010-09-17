8a5da96fa4a326e17402137f5ed44a16ae501df3
ses282 <ses282@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-17 23:47:25 +0000

    Updated plugin description
    git-svn-id: https://plugins.svn.wordpress.org/instant-band-site-by-nimbit/trunk@290830 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- instant-band-site-by-nimbit.php
+++ instant-band-site-by-nimbit.php
@@ -2,8 +2,8 @@
 /*
  Plugin Name: Instant Band Site by Nimbit
  Plugin URI: http://wordpress.org/extend/plugins/instant-band-site-by-nimbit/
- Description: With four easy steps this plugin will set you up with a complete site for your music.  Once You are done you will have combined the smartest website software with the best music business platform.
- Version: 0.2.1
+ Description: With the Nimbit Instant Band Site plug-in, it's easier than ever to create and maintain and artist website that provides the perfect showcase and storefront to connect with fans.
+ Version: 0.2.2
  Author: Nimbit
  Author URI:
  License: GPL2
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link:
 Tags: music, nimbit, band, band sites, band websites, band store, store, sell, digital, music, sell digital music
 Requires at least: 3.0
 Tested up to: 3.0
-Stable tag: 0.2.1
+Stable tag: 0.2.2
 
 Easily create a complete artist-band website with fan signup, gig listings, storefront and more...
 
@@ -88,3 +88,6 @@ Bug fixes & verbiage updates
 
 = 0.2.1 =
 Fixed foreground in transparent mode
+
+= 0.2.2 =
+Updated plugin description
